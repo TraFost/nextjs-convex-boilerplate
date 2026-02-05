fee758a5713c876a7d3218839fa1798db473ee2b
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2026-02-05 11:26:46 +0700

    refactor: replace analyzeModel with mainModel

--- app/lib/ai/ai-model.lib.ts
+++ app/lib/ai/ai-model.lib.ts
@@ -1,19 +1,16 @@
 import { openRouter, openRouterAsOpenAI } from "@/configs/ai.config";
 import { env } from "@/configs/env.config";
 
-export const analyzeModel = openRouter(
-	env.NEXT_PUBLIC_OPENROUTER_ANALYZE_MODEL,
-	{
-		logprobs: true,
-		reasoning: {
-			effort: "medium",
-			enabled: true,
-		},
-		usage: {
-			include: true,
-		},
+export const mainModel = openRouter(env.NEXT_PUBLIC_OPENROUTER_MAIN_MODEL, {
+	logprobs: true,
+	// reasoning: {
+	// 	effort: "medium",
+	// 	enabled: true,
+	// },
+	usage: {
+		include: true,
 	},
-);
+});
 
 export const embeddingModel = openRouterAsOpenAI.embedding(
 	env.NEXT_PUBLIC_OPENROUTER_EMBEDDING_MODEL, // 1024-dimensional embeddings
